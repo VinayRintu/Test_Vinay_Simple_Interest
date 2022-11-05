a23d93154c04ab633047bbbdefb31a0fe0faa3ec
Rintu <Rintu@VINDUJA>
2022-11-05 15:52:21 +0530

    Add project files.

---
+++ Test_Vinay_Simple_Interest/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Test_Vinay_Simple_Interest;
+
+Console.WriteLine("Hello, World!");
+
+Simple_Interest objSimple = new Simple_Interest();
+Console.WriteLine("Enter Principle Amount");
+objSimple.PrincipleAmount = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter Time");
+objSimple.Time = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter Rate Of Interest");
+objSimple.RateOfInterest = double.Parse(Console.ReadLine());
+
+objSimple.CalculateSimpleInterest();
---
+++ Test_Vinay_Simple_Interest/Simple_Interest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Vinay_Simple_Interest
+{
+    public class Simple_Interest
+    {
+        public double PrincipleAmount { get; set; }
+        public double Time { get; set; }
+        public double RateOfInterest { get; set; }
+
+        public void CalculateSimpleInterest()
+        {
+            double simpleInterest = (PrincipleAmount * Time * RateOfInterest) / 100;
+            Console.WriteLine("Simple Interest for given Credentials  : "+ simpleInterest);
+
+            double totalAmount=PrincipleAmount+simpleInterest;
+            Console.WriteLine("Total amount to return : "+ totalAmount);
+        }
+    }
+}
